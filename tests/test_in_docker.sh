8fefcdf79322db876b1378e974162e8df96cf817
timerke <60771762+Doom-ux@users.noreply.github.com>
2025-03-13 15:41:31 +0700

    Create test_in_docker.sh

---
+++ tests/test_in_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Launch QEMU in background
+qemu-system-arm -m 256 -M romulus-bmc -nographic -drive file=/home/runner/work/pytest-webui/pytest-webui/romulus/obmc-phosphor-image-romulus-20250214213550.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu &
+QEMU_PID=$!
+
+SERIAL_DEVICE=/dev/pts/0
+
+# Wait until serial device is registered and QEMU is ranning.
+while [ ! -e "$SERIAL_DEVICE" ]; do
+    if ! kill -0 "$QEMU_PID" 2>/dev/null; then
+        echo "QEMU exited unexpectedly."
+        exit 1
+    fi
+    sleep 0.1s
+done
+
+# Run pytest
+pytest tests/test_obmc1.py
