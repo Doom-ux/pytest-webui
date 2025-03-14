edbd6cf97d6dd2bbdfc30b29355e29667d412c33
timerke <60771762+Doom-ux@users.noreply.github.com>
2025-03-14 13:33:55 +0700

    Change option "file" of qemu-system-arm in test_in_docker.sh

--- tests/test_in_docker.sh
+++ tests/test_in_docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Launch QEMU in background
-qemu-system-arm -m 256 -M romulus-bmc -nographic -drive file=/home/runner/work/pytest-webui/pytest-webui/romulus/obmc-phosphor-image-romulus-20250314044154.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu &
+qemu-system-arm -m 256 -M romulus-bmc -nographic -drive file=/romulus/obmc-phosphor-image-romulus-20250214213550.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu &
 QEMU_PID=$!
 
 SERIAL_DEVICE=/dev/pts/0
