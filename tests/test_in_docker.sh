d936e5ad01b15eb073054b797bb311d8ad9bc6f0
timerke <60771762+Doom-ux@users.noreply.github.com>
2025-03-27 16:08:33 +0700

    Remove option '-nographic' in qemu-system command of  test_in_docker.sh

--- tests/test_in_docker.sh
+++ tests/test_in_docker.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Launch QEMU in background
-qemu-system-arm -m 256 -M romulus-bmc -nographic -daemonize -drive file=./romulus/obmc-phosphor-image-romulus-20250214213550.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu
+qemu-system-arm -m 256 -M romulus-bmc -daemonize -drive file=./romulus/obmc-phosphor-image-romulus-20250214213550.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu
 QEMU_PID=$!
 
 #SERIAL_DEVICE=/dev/ttyS4
@@ -12,6 +12,8 @@ if ! kill -0 "$QEMU_PID" 2>/dev/null; then
     echo "QEMU exited unexpectedly."
     exit 1
 fi
+
+sleep 7s
 
 # Wait until serial device is registered and QEMU is ranning.
 
@@ -22,7 +24,6 @@ fi
 #    fi
 #    sleep 0.1s
 #done
-
 
 # Run pytest
 pytest test_obmc1.py
