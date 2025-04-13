5ddd04c17598d9842219931b80fb45f7cb6b8ade
timerke <60771762+Doom-ux@users.noreply.github.com>
2025-04-13 09:57:33 +0700

    Update: qemu silent mode in test_in_docker.sh

--- tests/test_in_docker.sh
+++ tests/test_in_docker.sh
@@ -3,7 +3,9 @@
 set -e
 
 # Launch QEMU in background
-qemu-system-arm -m 256 -M romulus-bmc -daemonize -drive file=./romulus/obmc-phosphor-image-romulus-20250214213550.static.mtd,format=raw,if=mtd -net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu
+qemu-system-arm -m 256 -M romulus-bmc -nographic -serial none -monitor none \
+-drive file=./romulus/obmc-phosphor-image-romulus-20250214213550.static.mtd,format=raw,if=mtd \
+-net nic -net user,hostfwd=:0.0.0.0:2222-:22,hostfwd=:0.0.0.0:2443-:443,hostfwd=udp:0.0.0.0:2623-:623,hostname=qemu &
 QEMU_PID=$!
 
 #SERIAL_DEVICE=/dev/ttyS4
@@ -13,7 +15,7 @@ if ! kill -0 "$QEMU_PID" 2>/dev/null; then
     exit 1
 fi
 
-sleep 7s
+sleep 95s
 
 # Wait until serial device is registered and QEMU is ranning.
 
